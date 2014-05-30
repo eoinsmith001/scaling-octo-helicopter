0d32041e39cf93f138b4cfe9d812c39602e51280
Eoin Smith <eoinsmith001@gmail.com>
2014-05-30 13:32:55 +1000

    ensured MainCtrl does not reload on search

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,8 @@ angular.module('todoApp', [
     $routeProvider
       .when('/', {
         templateUrl: 'partials/main',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        reloadOnSearch: false
       })
       .otherwise({
         redirectTo: '/'
